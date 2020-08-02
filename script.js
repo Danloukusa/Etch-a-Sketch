d334f2f07e23618aa4d04f7ca41705f5a01a40a3
Danloukusa <Daniel.Loukusa@hotmail.com>
2020-08-02 07:17:44 -0400

    improve grid. add etch-a-sketch function.

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-
-
-
 function myFunction(){
     let i;
     for(i = 0; i < 256; i++)
     {
         const insert = document.createElement("p");
+        insert.className = "innerCell";
+        insert.addEventListener("mouseover", rat);
         container.appendChild(insert);
-        console.log(i);
     }
+}
 
-    
+function rat(){
+    this.className = "blue";
 }
 
 myFunction();
