6d2b3b9698803add4d5a00781b0fced7635d3f0a
Danloukusa <Daniel.Loukusa@hotmail.com>
2020-08-02 05:51:19 -0400

    make HTML skeleton. Insert 16x16 grid through JS.

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+
+function myFunction(){
+    let i;
+    for(i = 0; i < 256; i++)
+    {
+        const insert = document.createElement("p");
+        container.appendChild(insert);
+        console.log(i);
+    }
+
+    
+}
+
+myFunction();
