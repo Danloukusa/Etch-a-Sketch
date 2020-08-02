f63f416ed2efc483bbc168770972fc320392d178
Danloukusa <Daniel.Loukusa@hotmail.com>
2020-08-02 07:23:31 -0400

    change p to div per instructions.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function myFunction(){
     let i;
     for(i = 0; i < 256; i++)
     {
-        const insert = document.createElement("p");
+        const insert = document.createElement("div");
         insert.className = "innerCell";
         insert.addEventListener("mouseover", rat);
         container.appendChild(insert);
